bf5886b40ef306eb86566fc8a83811e150a8f479
jvcorado <jhonisilva545@gmail.com>
2025-06-15 14:46:48 -0300

    build vercel plans

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,7 @@
+
+export const dynamic = 'force-dynamic';
+
+
 import { NextAuthOptions } from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
